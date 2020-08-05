0920edc8601ac595a13dde204d55ac6a054c8fb8
erickpolin <erick.polin@gmail.com>
2020-08-05 14:03:04 +0200

    a

--- Creación base 2018.R
+++ Creación base 2018.R
@@ -12,7 +12,7 @@ library(tidyverse)
 options(survey.lonely.psu="adjust")
 
 #reading the data
-setwd("C:/Users/Erick/Dropbox/GIC/2018")
+setwd("C:/Users/Erick/Dropbox/GIC/GITHUB2018/GIC/ENIGH 2018/ENIGH 2018")
 Conc<-read.dbf("concentradohogar.dbf",as.is = T)
 
 #Keeping Variables of interest
@@ -31,8 +31,8 @@ Poblacion <- Poblacion [ c("folioviv", "foliohog", "numren", "parentesco","habla
 parentescos<-c(101,102,201,202,203,204,205,601,602,606,607,608,615,616)
 
 Poblacion<- Poblacion%>%
-  mutate(HogarIndigena=ifelse(parentesco%in%parentescos&hablaind==1|parentesco%in%parentescos&comprenind==1|parentesco%in%parentescos&etnia==1,1,0))
-
+  mutate(HogarIndigena=ifelse(parentesco%in%parentescos&hablaind==1,1,0))
+#|parentesco%in%parentescos&comprenind==1|parentesco%in%parentescos&etnia==1
 HogaresIndigenas<-Poblacion %>%
   group_by(folioviv,foliohog)%>%
   summarize(HogarIndigena=mean(HogarIndigena))
